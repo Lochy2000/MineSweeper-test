62f64305dcf8c7530fd835a9d69833635ffb9559
lochy2000 <lochlann_oht@hotmail.com>
2024-08-02 10:30:26 +0100

    added instructions top of game

--- run.py
+++ run.py
@@ -6,13 +6,11 @@ init(autoreset=True)
 
 #  ASCII art for "MINESWEEPER!" title
 ASCII_ART = r"""
- ___ ___  ____  ____     ___  _____ __    __    ___    ___  ____   ___  ____  
-|   |   ||    ||    \   /  _]/ ___/|  |__|  |  /  _]  /  _]|    \ /  _]|    \ 
-| _   _ | |  | |  _  | /  [_(   \_ |  |  |  | /  [_  /  [_ |  o  )  [_ |  D  )
-|  \_/  | |  | |  |  ||    _]\__  ||  |  |  ||    _]|    _]|   _/    _]|    / 
-|   |   | |  | |  |  ||   [_ /  \ ||  `  '  ||   [_ |   [_ |  | |   [_ |    \ 
-|   |   | |  | |  |  ||     |\    | \      / |     ||     ||  | |     ||  .  \
-|___|___||____||__|__||_____| \___|  \_/\_/  |_____||_____||__| |_____||__|\_|
+███    ███ ██ ███    ██ ███████ ███████ ██     ██ ███████ ███████ ██████  ███████ ██████  
+████  ████ ██ ████   ██ ██      ██      ██     ██ ██      ██      ██   ██ ██      ██   ██ 
+██ ████ ██ ██ ██ ██  ██ █████   ███████ ██  █  ██ █████   █████   ██████  █████   ██████  
+██  ██  ██ ██ ██  ██ ██ ██           ██ ██ ███ ██ ██      ██      ██      ██      ██   ██ 
+██      ██ ██ ██   ████ ███████ ███████  ███ ███  ███████ ███████ ██      ███████ ██   ██
             """ 
 #  ASCII art for "GAME OVER!!!"
 GAME_OVER_ART = r"""
@@ -266,6 +264,7 @@ def play(dim_size=10, num_bombs=10):
     Step 4: repeat steps 2 and 3a/b until there are no more places to dig -> VICTORY!
     """
     print(Fore.LIGHTGREEN_EX + ASCII_ART + Style.RESET_ALL)  # Display the ASCII art title
+    print(Fore.LIGHTGREEN_EX + "GAME INTRUCTIONS\n Guess all locations without bombs \n Input: row first followed by a comma then the collumn \n To place a flag put f infront of location ex: f 2,3 \n If you want to dig where a flag is, you must remove the flag first, by typing the location again with f infront ex: f 2,3.")
     board = Board(dim_size, num_bombs)
     safe = True
 
